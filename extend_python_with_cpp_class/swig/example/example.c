e3d6c5da51a74c8de3b365b791a7fbc6f6b443ab
Hu Wenchao <zjuhwc@gmail.com>
2015-11-25 16:40:09 +0800

    update example by official doc

--- extend_python_with_cpp_class/swig/example/example.c
+++ extend_python_with_cpp_class/swig/example/example.c
@@ -1,22 +1,16 @@
- /* File : example.c */
+/* File: example.c */
 
- #include <time.h>
- double My_variable = 3.0;
+#include "example.h"
 
- int fact(int n) {
-     if (n <= 1) return 1;
-     else return n*fact(n-1);
- }
-
- int my_mod(int x, int y) {
-     return (x%y);
- }
-
- char *get_time()
- {
-     time_t ltime;
-     time(&ltime);
-     return ctime(&ltime);
- }
-
-
+int fact(int n) {
+    if (n < 0){ /* This should probably return an error, but this is simpler */
+        return 0;
+    }
+    if (n == 0) {
+        return 1;
+    }
+    else {
+        /* testing for overflow would be a good idea here */
+        return n * fact(n-1);
+    }
+}
--- extend_python_with_cpp_class/swig/example/example.i
+++ extend_python_with_cpp_class/swig/example/example.i
@@ -1,14 +1,9 @@
- /* example.i */
- %module example
- %{
- /* Put header files here or function declarations like below */
- extern double My_variable;
- extern int fact(int n);
- extern int my_mod(int x, int y);
- extern char *get_time();
- %}
+/* File: example.i */
+%module example
 
- extern double My_variable;
- extern int fact(int n);
- extern int my_mod(int x, int y);
- extern char *get_time();
+%{
+#define SWIG_FILE_WITH_INIT
+#include "example.h"
+%}
+
+int fact(int n);
